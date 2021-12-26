002b7f71c869b7de1435ec800224ed8e1d73ccd4
FelipeReis <felipe.reis@icarotech.com>
2021-12-26 19:27:12 -0300

    primeiro dash
    rpm, speed, gear

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # dash-formulaone-2021
- painel for formula one game 2021
+Speed Painel para o jogo formula 1 2021
+
+## Mostrador
+
+Traz o RPM, Velocidade em KM/H e Marcha
+
+## Uso
+
+Necessario ter o NodeJS 
+
+Para executar é só na pasta raiz rodar o comando 
+```
+node index.js
+```
+No seu celular ou tablet abra o navegador e digite o ip do compatador na porta 3000
+>exemplo -> 192.168.1.1:3000
--- index.js
+++ index.js
@@ -1,35 +1,48 @@
-import F1TelemetryClient from "f1-2021-udp";
-import http from "http"
+const express = require('express')
+const http = require("http")
+const F1TelemetryClient = require("f1-2021-udp")
+const { Server } = require("socket.io");
 
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+// arquivos estaticos
+app.use('/static', express.static(__dirname + '/public'));
+
+io.on('connection', (socket) => {
+    console.log('Front-End conectado');
+    socket.on('disconnect', () => {
+        console.log('Front-End Disconectado');
+      });
+});
+
+function sendToFront(speed, gear, rpm) {
+    io.emit('dash', { speed: speed, gear: gear, rpm: rpm });
+}
+
+
+// UDP listener e parser do jogo f1 2021
 const client = new F1TelemetryClient.F1TelemetryClient();
-
-
-// client.on('motion', function (data) {
-//     //console.log('data');
-//     console.log(data.m_speed);
-// })
-
 client.on('carTelemetry', function (data) {
-    //console.log('carTelemetry');
-    // console.log(data.m_carTelemetryData[0].m_speed);
-    let speed = data.m_carTelemetryData[0].m_speed
-    let rpm = data.m_carTelemetryData[0].m_engineRPM
-    let gear = data.m_carTelemetryData[0].m_gear
 
-    http.get(`http://localhost:1880/formula?speed=${speed}&rpm=${rpm}&gear=${gear}`, (resp) => {
-        let data = '';
-        // A chunk of data has been received.
-        resp.on('data', (chunk) => {
-            data += chunk;
-        });
-        // The whole response has been received. Print out the result.
-        resp.on('end', () => {
-            console.log(JSON.parse(data).explanation);
-        });
-    }).on("error", (err) => {
-        console.log("Error: " + err.message);
-    });
+    let speed = data.m_carTelemetryData[0].m_speed
+    let gear = data.m_carTelemetryData[0].m_gear
+    let rpm = data.m_carTelemetryData[0].m_engineRPM   
+
+    sendToFront(speed, gear, rpm)
 })
 
 
+// inicia o listener do jogo
 client.start();
+
+// inicia servidor express
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
+
+
